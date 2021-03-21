555ac4ad0f175d5844358e662801e7463c6d66b2
Urusa <usaiyed.9127@gmail.com>
2021-03-20 20:54:34 -0600

    image removed

--- src/components/DisplayAffirmation/DisplayAffirmation.js
+++ src/components/DisplayAffirmation/DisplayAffirmation.js
@@ -4,14 +4,16 @@ import './DisplayAffirmation.css'
 function DisplayAffirmation({displayAffirm}){
 	return(
 		<div className = 'container display-affirmation'>
+			<article className='center mw5 mw6-ns hidden ba mv4'>
 			<h1 className="">Affirmations to change mindset</h1>
 		  		<div className="">
 		    		<p 
-		    		className="" 
+		    		className="f6 f5-ns lh-copy measure mv0" 
 		    		id = 'affirmations'>
 		     			{displayAffirm}
 		    		</p>
 		  		</div>
+		  	</article>	
 		</div>
 
 	)
